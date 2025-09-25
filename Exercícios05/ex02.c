8811c9f200c6600db9e93fc76d39a8417b8c3fb4
Bruno-Bonato <brunobonatosc@hotmail.com>
2025-09-25 13:45:02 -0300

    Update ex02.c (Ponteiros)

--- Exercícios05/ex02.c
+++ Exercícios05/ex02.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-int compra(int* conta, int valor){
+void compra(int* conta, int valor){
     *conta = *conta - valor;
-    return *conta;
 }
 
 int main(void){
     int minha_conta1, minha_conta2, valor = 500;
     int* conta;
-   
 
     printf("Qual o saldo da primeira e da segunda conta? ");
     scanf("%d %d", &minha_conta1, &minha_conta2);
 
     if (minha_conta1 > minha_conta2){
         conta = &minha_conta1;
-        printf("O saldo das contas é: %d e %d\n", compra(conta, valor),minha_conta2);
+        compra(conta, valor);
+        printf("O saldo das contas é: %d e %d\n", minha_conta1,minha_conta2);
     } else {
         conta = &minha_conta2;
-        printf("O saldo das contas é: %d e %d\n", minha_conta1, compra(conta, valor));
+        compra(conta, valor);
+        printf("O saldo das contas é: %d e %d\n", minha_conta1, minha_conta2);
     }
 
     return 0;
